094f258d65eb0e56e408a9f1e5279d5e24063c7f
Marpss11 <mariamparedes23@gmail.com>
2025-04-28 09:45:51 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/',(req,res)=> {
+  res.send('Hola desde la nube con Express');
+});
+
+app.listen(port, () => {
+  console.log('Servidor escuchando en http://localhost:${port}');
+
+
